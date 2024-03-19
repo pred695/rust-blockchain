6685b82da9b51aa61749f56e22a5cf086f920c44
Zaid <predcodes695@gmail.com>
2024-03-19 23:14:02 +0530

    add: Added the proof of work algorithm and utlity functions

--- rust-blockchain/src/block.rs
+++ rust-blockchain/src/block.rs
@@ -1,24 +1,33 @@
-use sha2::{Digest, Sha256};
+use crate::proof;
 pub struct Block {
     pub hash: Vec<u8>,
     pub data: Vec<u8>,
     pub prev_hash: Vec<u8>,
+    pub nonce: u64,
 }
 
 impl Block {
-    pub fn derive_hash(&mut self) {
-        let info = [self.data.clone(), self.prev_hash.clone()].concat();
-        let mut hasher = Sha256::new();
-        hasher.update(&info);
-        self.hash = hasher.finalize().to_vec();
+    pub fn create_block(data: String, prev_hash: Vec<u8>) -> Self {
+        let mut block = Block{
+            hash: Vec::new(),
+            data: data.as_bytes().to_vec(),
+            prev_hash,
+            nonce: 0,
+        };
+        let mut proof = proof::ProofOfWork::new_proof(&block);
+        let (nonce, hash) = proof.run();
+        block.nonce = nonce;
+        block.hash = hash;
+        return block;
     }
+    pub fn genesis() -> Self {
+        let data = String::from("Genesis block");
+        let prev_hash = Vec::new();
+        let genesis_block = Self::create_block(data, prev_hash);//3) Use the create function to create genesis block
+        return genesis_block;
+     }
+     
 }
 
 
-pub fn genesis() -> Block {
-    Block {
-        hash: vec![0; 32],
-        data: "First block".as_bytes().to_vec(),
-        prev_hash: Vec::new(),
-    }
-}
+
--- rust-blockchain/src/blockchain.rs
+++ rust-blockchain/src/blockchain.rs
@@ -1,21 +1,17 @@
-use crate::block::{Block, genesis};
+use crate::block::Block;
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
 
 impl Blockchain {
     pub fn add_block(&mut self, data: Vec<u8>) {
-        let prev_hash = self.blocks.last().unwrap().hash.clone();
-        let mut new_block = Block {
-            data,
-            prev_hash,
-            hash: Vec::new(),
-        };
-        new_block.derive_hash();
+        let prev_block = &self.blocks[self.blocks.len() - 1];
+        let data = String::from_utf8(data).unwrap();
+        let new_block = Block::create_block(data, prev_block.hash.clone());//2) Use the create block function
         self.blocks.push(new_block);
     }
     pub fn init_blockchain(&mut self) {
-        let genesis_block = genesis();
+        let genesis_block = Block::genesis();
         self.blocks.push(genesis_block);
     }
 }
